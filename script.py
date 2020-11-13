1e451491f7c32b5d28a0d2ca9d7a77a66b2ee2a8
Alireza Alizadegan <a.r.alizadegan@gmail.com>
2020-11-12 20:45:09 -0800

    Update script.py

--- script.py
+++ script.py
@@ -15,7 +15,7 @@ test_df.fillna(test_df.median(), inplace=True)
 scaler = StandardScaler()
 test_trans_df = scaler.fit_transform(test_df)
 
-best_model = tf.keras.models.load_model(r'C:\Users\arali\Desktop\Audicia\Test')
+best_model = tf.keras.models.load_model(r'C:\Users\arali\Desktop\hotel_website')
 n_clicks_array = best_model.predict(test_trans_df)
 n_clicks_array[n_clicks_array < 0] = 0
 floor_n_clicks_array = np.floor(n_clicks_array) 
